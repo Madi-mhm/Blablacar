6105f84f108e6a44655858e5617d5f1e337a0aae
Mahdi <mahdi.unic1@gmail.com>
2023-05-24 16:57:50 +0200

    end of the day

--- src/Controller/RideController.php
+++ src/Controller/RideController.php
@@ -11,8 +11,35 @@ class RideController extends AbstractController
     #[Route('/', name: 'app_ride')]
     public function index(): Response
     {
-        return $this->render('ride/homePage.html.twig', [
+        return $this->render('pages/homePage.html.twig', [
             'controller_name' => 'RideController',
         ]);
     }
+
+    #[Route('/booking', name: 'booking_page')]
+    public function booking(): Response
+    {
+        return $this->render('pages/booking.html.twig', [
+            'controller_name' => 'RideController',
+        ]);
+    }
+
+    #[Route('/publish', name: 'publish_page')]
+    public function publish(): Response
+    {
+        return $this->render('pages/publish.html.twig', [
+            'controller_name' => 'RideController',
+        ]);
+    }
+
+    #[Route('/login', name: 'login_page')]
+    public function login(): Response
+    {
+        return $this->render('pages/login.html.twig', [
+            'controller_name' => 'RideController',
+        ]);
+    }
+
+
 }
+
